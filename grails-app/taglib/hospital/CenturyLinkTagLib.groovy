9ab2232b19e4430185d4bb79391a882fb5164e2a
Jonathan Larson <macman247@gmail.com>
2012-10-02 13:41:12 -0500

    Started a tag library
    Started on the grid/column tags

---
+++ grails-app/taglib/hospital/CenturyLinkTagLib.groovy
@@ -0,0 +1,52 @@
+package hospital
+
+import grails.converters.JSON
+
+class CenturyLinkTagLib {
+    def grid = { attrs, body ->
+
+        // param attributes
+        if (!attrs.containsKey('name')) {
+            throwTagError('The grid tag must have a name attribute')
+        }
+        if (!attrs.containsKey('data')) {
+            throwTagError('The grid tag must have a data attribute')
+        }
+        if (!attrs.containsKey('id')) {
+            attrs.id = attrs.name
+        }
+
+        pageScope.gridColumns = []
+
+        // run what we have inside our grid, but don't output (no need)
+        body()
+
+        out << r.script() {
+            out << """\
+                \$(function(){
+                    \$('#${attrs.id}').grid({
+                        data: ${ attrs.data.encodeAsJSON() }
+                        , columns: ${ pageScope.gridColumns.encodeAsJSON() }
+                    });
+                });
+            """
+        }
+        out << "<div id=\"${ attrs.id }\"></div>"
+    }
+
+    def column = { attrs, body ->
+
+        // param attributes
+        if (!attrs.containsKey('name')) {
+            throwTagError('The column tag must have a name attribute')
+        }
+        if (!attrs.containsKey('label')) {
+            attrs.label = attrs.name
+        }
+        if (!attrs.containsKey('renderer')) {
+            attrs.renderer = 'string'
+        }
+
+        pageScope.gridColumns.add(attrs)
+    }
+}
---
+++ test/unit/hospital/CenturyLinkTagLibTests.groovy
@@ -0,0 +1,17 @@
+package hospital
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(CenturyLinkTagLib)
+class CenturyLinkTagLibTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
