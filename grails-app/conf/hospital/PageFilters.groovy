be98137dc7919fb459254ef60f4903c1ae051677
Jonathan Larson <macman247@gmail.com>
2012-10-02 13:41:39 -0500

    Added an experimental filter
    Commented out for now due to no use case.

--- grails-app/conf/hospital/PageFilters.groovy
+++ grails-app/conf/hospital/PageFilters.groovy
@@ -6,6 +6,18 @@ class PageFilters {
 
     def filters = {
         all(controller:'*', action:'*') {
+            before = {
+                /* very experimental code for getting the columns of a linked domain class - JWL
+                if (controllerName != null) {
+                    def controllerChars = controllerName.toCharArray()
+                    controllerChars[0] = controllerChars[0].toUpperCase()
+                    def controllerDomainClass = grailsApplication.getDomainClass('hospital.' + controllerChars)
+                    if (controllerDomainClass != null) {
+                        def domainColumns = controllerDomainClass.properties.collect { if (it.persistent) { it.name } }
+                    }
+                }
+                */
+            }
             after = { Map model ->
                 if (model != null) {
                     model.mainNav = navService.get()
