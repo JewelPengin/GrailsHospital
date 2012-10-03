a08b2bd70cd7d68d4f85fc184b84bbab64064207
Farouk Althlathini <mfarouk.tawfik@gmail.com>
2012-10-03 13:02:15 -0500

    added login info in bootstrap.groovy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,24 @@
+import com.centurylink.hospital.auth.Authority
+import com.centurylink.hospital.auth.Person
+import com.centurylink.hospital.auth.PersonAuthority
+
 class BootStrap {
 
+    def springSecurityService
+
     def init = { servletContext ->
+        if(!Person.count()){
+            createDate()
+        }
     }
     def destroy = {
+    }
+
+    public void createData(){
+        def userRole = new Authority(authority: 'ROLE_USER').save(failOnerror: true)
+        def password = springSecurityService.encodePassword('password')
+
+        def user = new Person(username:  'farouk', realname: 'farouk',password: password).save(failOnerror: true)
+        PersonAuthority.create user, userRole, true
     }
 }
