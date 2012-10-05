f1178083e0190fa9821c615efd4d7265e5bea033
Jonathan Larson <macman247@gmail.com>
2012-10-05 15:33:47 -0500

    Fixing an issue with the GORM constraint on Room

--- grails-app/domain/com/centurylink/hospital/Room.groovy
+++ grails-app/domain/com/centurylink/hospital/Room.groovy
@@ -5,7 +5,7 @@ class Room {
 	static hasMany = [staff: Nurse, occupingPatient: Patient]
 
 	static constraints = {
-		occupier nullable: true // Representing the circle on the room number entity link to patient
+		occupingPatient nullable: true // Representing the circle on the room number entity link to patient
 		staff nullable: false // Representing the solid dash on the Nurse link
 	}
 
