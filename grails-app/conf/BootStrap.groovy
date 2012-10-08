d6c5e72f7d3c1942d25f5c2996b28aaefaed2863
Jonathan Larson <macman247@gmail.com>
2012-10-08 09:09:04 -0500

    Formatting the bootstrap
    Also fixed an issue where the save was in the wrong place.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,19 +15,23 @@ class BootStrap {
     public void createData(){
         def userRole = Authority.findByAuthority('ROLE_USER') ?: new Authority(authority: 'ROLE_USER').save(flush:true)
         def adminRole = Authority.findByAuthority('ROLE_ADMIN')?: new Authority(authority: 'ROLE_ADMIN').save(flush: true)
-        [dave: [name: 'Dave Gunawan', role: userRole], jon: [name: 'Jonathan Larson', role: adminRole], brad: [name: 'Bradley Rhoades', role: userRole],farouk:[name:'Farouk Althlathini',role: adminRole]].each{username,userData ->
+        [
+            dave: [name: 'Dave Gunawan', role: userRole]
+            , jonathan: [name: 'Jonathan Larson', role: adminRole]
+            , brad: [name: 'Bradley Rhoades', role: userRole]
+            , farouk:[name:'Farouk Althlathini', role: adminRole]
+        ].each{ username, userData ->
             def userParams = [
                 username: username
                 , enabled: true
                 , realname: userData.name
                 , password: '1234'
             ]
-            def user = Person.findByUsername(username)?: new Person(userParams)
+            def user = Person.findByUsername(username)?: new Person(userParams).save(flush: true)
 
             if (!user.authorities.contains(userData.role)){
                 PersonAuthority.create user, userData.role, true
             }
-            user.save(flush:true)
         }
 
         assert Person.count() == 4
