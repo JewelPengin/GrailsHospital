2d59e4750052ec8d162ed919b39f62d518d6a7c7
Jonathan Larson <macman247@gmail.com>
2012-10-08 08:58:38 -0500

    Actually fixing datasource.groovy

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -11,37 +11,6 @@ hibernate {
 }
 // environment specific settings
 environments {
-<<<<<<< HEAD
-    development {
-        dataSource {
-            dbCreate = "create" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:file:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
-    }
-    production {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-            pooled = true
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=true
-               validationQuery="SELECT 1"
-            }
-        }
-    }
-=======
 	development {
 		dataSource {
 			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
@@ -71,5 +40,4 @@ environments {
 			}
 		}
 	}
->>>>>>> f66eb85c968ad651ba860790a7adfd751ee407da
 }
